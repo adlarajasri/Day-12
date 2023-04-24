db68ef92f74a4eeda0c75889e1aa7869c824820c
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-04-24 10:56:48 +0530

    Add files via upload

---
+++ Finally/Finally/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 2; int[] a = new int[5];
+            try
+            {
+                int i = 10 / b;
+                a[0] = 9;
+            }
+            catch (DivideByZeroException e) {
+                Console.WriteLine("Divide by zero error");
+            }
+            catch (IndexOutOfRangeException e) {
+                Console.WriteLine("Index out of bound");
+            }
+            finally {
+                Console.WriteLine("finally");
+            }
+            Console.WriteLine("Remaining program");
+        }
+    }
+}
