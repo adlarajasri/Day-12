27ff72b5ad142f26bd0068d08b3c9e504b5525f9
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-04-24 10:55:31 +0530

    Add files via upload

---
+++ Customer/Customer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public class Class1
+
+        {
+
+            static void Main(string[] args)
+
+            {
+
+                customer c = new customer("Rahul", 2453, 500);
+
+                try
+
+                {
+
+                    c.withdraw(450);
+
+                }
+
+                ca tch(bankexception e)
+
+                {
+
+                    Console.WriteLine("Transaction Failed ");
+
+                    e.inform();
+
+                }
+
+            }
+        
+
+    }
+}
+    
+
